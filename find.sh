d9b4ca99f10ae72d5585b1ead610086cd593ac04
Piotr Schuetz <piotr.schuetz@gmail.com>
2016-07-06 13:31:52 +0200

    new find with date range

--- find.sh
+++ find.sh
@@ -1,2 +1,5 @@
 #search for files in . directory from last 120 min and sum their disk usage
 find . -type f -mmin -120 -print0 | du -hc --files0-from - | tail -n 1
+
+#search for files in . directory from date to date and sum their disk usage
+find /arch/logi/comarch_old/testy -type f -newermt "2016-06-28 00:00:00" \! -newermt "2016-06-29 23:59:59" -print0 | du -hc --files0-from - | tail -n 1
